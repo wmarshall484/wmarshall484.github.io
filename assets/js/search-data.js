06799562b22f12cb76fba48dce0c004b7a98bdbe
wmarshall484 <wcm214@gmail.com>
2025-11-02 05:01:09 +0000

    Deploying to gh-pages from @ wmarshall484/wmarshall484.github.io@b1aff668ca23b8063ce3341423210a48513a8eab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/about/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-a-post-with-math",
+        
+          title: "a post with math",
+        
+        description: "an example of a blog post with some math",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/math/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
